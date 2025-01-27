aa0297860d46ca36377a73e378842028c7730184
fh1798 <kjrychie@gmail.com>
2025-01-27 14:07:36 +0100

    R02 Mexico 2025 added

--- season11_data.R
+++ season11_data.R
@@ -32,7 +32,9 @@ team_color_fct = function(){
 
 laps_read_list_fct = function(){
   laps_read_list <- data.frame(c("R01" = 
-                                   "data/Season11/R01_SaoPaulo/23_R01 Analysis_Race.csv"))
+                                   "data/Season11/R01_SaoPaulo/23_R01 Analysis_Race.csv",
+                                 "R02" = 
+                                   "data/Season11/R02_Mexico/23_R02 Analysis_Race.csv"))
   colnames(laps_read_list) <- c("path_laps")
   
   return(laps_read_list)
@@ -40,7 +42,9 @@ laps_read_list_fct = function(){
 
 result_read_list_fct = function(){
   result_read_list <- data.frame(c("R01" =
-                "data/Season11/R01_SaoPaulo/03_R01 Classification_Race.csv"))
+                "data/Season11/R01_SaoPaulo/03_R01 Classification_Race.csv",
+                "R02" =
+                  "data/Season11/R02_Mexico/03_R02 Classification_Race.csv"))
   colnames(result_read_list) <- c("path_result")
   
   return(result_read_list)
@@ -48,16 +52,18 @@ result_read_list_fct = function(){
 
 wdc_read_list_fct = function(){
   wdc_read_list <- data.frame(c("R01" =
-                                     "data/Season11/R01_SaoPaulo/Drivers Championship.csv"))
+                                     "data/Season11/R01_SaoPaulo/Drivers Championship.csv",
+                                "R02" =
+                                  "data/Season11/R02_Mexico/Drivers Championship.csv"))
   colnames(wdc_read_list) <- c("path_wdc")
   
   return(wdc_read_list)
 }
 
 sector_len_list_fct = function(){
-  S1_len = c("R01" =  980)
-  S2_len = c("R01" = 2160)
-  S3_len = c("R01" = 2642)
+  S1_len = c("R01" =  980, "R02" =  947)
+  S2_len = c("R01" = 2160, "R02" =  1846)
+  S3_len = c("R01" = 2642, "R02" =  2630)
   sector_len_list <- data.frame(S1_len, S2_len, S3_len)
   
   return(sector_len_list)
@@ -69,9 +75,9 @@ race_data_fct = function(){
   sector_len_list <- sector_len_list_fct()
   wdc_read_list <- wdc_read_list_fct()
   
-  round_number <- c("R01" = "R01_")
-  folder_name <- c("R01" = "R01_SaoPaulo")
-  race_name <- c("R01" = "SaoPaulo")
+  round_number <- c("R01" = "R01_", "R02" = "R02_")
+  folder_name <- c("R01" = "R01_SaoPaulo", "R02" = "R02_Mexico")
+  race_name <- c("R01" = "SaoPaulo", "R02" = "Mexico")
   
   race_data <- data.frame(round_number, folder_name, race_name,
                           laps_read_list, result_read_list, wdc_read_list)
